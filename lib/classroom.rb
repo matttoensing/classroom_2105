746056ef71e1ebc63770caa1293110e0950b54c1
Matt Toensing <matthew.toensing@gmail.com>
2021-05-12 15:04:51 -0600

    Include rspec tests and accompanying classroom file

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,31 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students
+    @students.map do |student|
+      student.upcase
+    end
+  end
+
+  def over_capacity?
+    if @students.length <= @capacity
+      return false
+    elsif @students.length > @capacity
+      return true
+    end
+  end
+
+  def kick_out
+    @students.shift
+  end
 end
--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -3,22 +3,22 @@ require './lib/classroom'
 
 describe Classroom do
   context 'Attributes' do
-    xit 'is a classroom' do
+    it 'is a classroom' do
       classroom = Classroom.new('History', 4)
       expect(classroom).to be_an_instance_of(Classroom)
     end
 
-    xit 'has a subject' do
+    it 'has a subject' do
       classroom = Classroom.new('History', 4)
       expect(classroom.subject).to eq('History')
     end
 
-    xit 'has a capacity' do
+    it 'has a capacity' do
       classroom = Classroom.new('History', 4)
       expect(classroom.capacity).to eq 4
     end
 
-    xit 'has no students by default' do
+    it 'has no students by default' do
       classroom = Classroom.new('History', 4)
       expect(classroom.students).to eq []
     end
@@ -27,7 +27,7 @@ describe Classroom do
   # Iteration 2
 
   context 'Students' do
-    xit 'returns a list of students' do
+    it 'returns a list of students' do
       classroom = Classroom.new('History', 4)
       classroom.add_student('Mike')
       classroom.add_student('Megan')
@@ -36,7 +36,7 @@ describe Classroom do
       expect(classroom.students).to eq ['Mike', 'Megan', 'Bob']
     end
 
-    xit 'returns a list of uppercased names' do
+    it 'returns a list of uppercased names' do
       classroom = Classroom.new('History', 4)
       classroom.add_student('Mike')
       classroom.add_student('Megan')
@@ -45,4 +45,73 @@ describe Classroom do
       expect(classroom.yell_at_students).to eq ['MIKE', 'MEGAN', 'BOB']
     end
   end
+
+  # Iteration 3
+
+  context 'Capacity' do
+    it "has a set limit" do
+      classroom = Classroom.new('History', 4)
+
+      classroom.add_student('Mike')
+      classroom.add_student('Megan')
+      classroom.add_student('Bob')
+
+      expect(classroom.over_capacity?).to be false
+
+      classroom.add_student('Eve')
+      classroom.add_student('Alice')
+
+      expect(classroom.over_capacity?).to be true
+    end
+  end
+
+  # Iteration 4
+
+  context 'Kick Out' do
+    it "can remove students" do
+      classroom = Classroom.new('History', 4)
+
+      classroom.add_student('Mike')
+      classroom.add_student('Megan')
+      classroom.add_student('Bob')
+      classroom.add_student('James')
+      classroom.add_student('Cat')
+      classroom.add_student('Alice')
+
+      classroom.kick_out
+
+      expect(classroom.over_capacity?).to be true
+
+      classroom.kick_out
+
+      expect(classroom.over_capacity?).to be false
+      expect(classroom.students).to eq(["Bob", "James", "Cat", "Alice"])
+    end
+  end
 end
+
+#   pry(main)> require './lib/classroom'
+# #=> true
+#
+# pry(main)> classroom = Classroom.new('History', 4)
+# => #<Classroom:0x00007fed7c11dbd8 @capacity=4, @subject="History", @students=[]>k
+#
+# pry(main)> classroom.add_student('Mike')
+# pry(main)> classroom.add_student('Megan')
+# pry(main)> classroom.add_student('Bob')
+# pry(main)> classroom.add_student('James')
+# pry(main)> classroom.add_student('Cat')
+# pry(main)> classroom.add_student('Alice')
+#
+# pry(main)> classroom.kick_out
+#
+# pry(main)> classroom.over_capacity?
+# => true
+#
+# pry(main)> classroom.kick_out
+#
+# pry(main)> classroom.over_capacity?
+# => false
+#
+# pry(main)> classroom.students
+# ["Bob", "James", "Cat", "Alice"]
